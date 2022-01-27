912632ce924ca167562ca03308fc4c4756f82ab3
teddy <teddyjh@protonmail.com>
2022-01-27 10:57:14 +0100

    add solution to 'Coordinate Transformation'

--- coordinate-transformation/coordinate-transformation.js
+++ coordinate-transformation/coordinate-transformation.js
@@ -15,7 +15,9 @@
  *  translated coordinate pair in the form [x, y]
  */
 export function translate2d(dx, dy) {
-  throw new Error('Implement the translate2d function');
+	return (x, y) => {
+		return [ x + dx, y + dy ];
+	};
 }
 
 /**
@@ -29,7 +31,9 @@ export function translate2d(dx, dy) {
  *  scaled coordinate pair in the form [x, y]
  */
 export function scale2d(sx, sy) {
-  throw new Error('Implement the scale2d function');
+	return (x, y) => {
+		return [ sx * x, sy * y ];
+	};
 }
 
 /**
@@ -43,7 +47,10 @@ export function scale2d(sx, sy) {
  *  transformed coordinate pair in the form [x, y]
  */
 export function composeTransform(f, g) {
-  throw new Error('Implement the composeTransform function');
+	return (x, y) => {
+		const [ x1, y1 ] = f(x, y);
+		return g(x1, y1);
+	};
 }
 
 /**
@@ -56,5 +63,17 @@ export function composeTransform(f, g) {
  *  if the arguments are the same on subsequent calls, or compute a new result if they are different.
  */
 export function memoizeTransform(f) {
-  throw new Error('Implement the memoizeTransform function');
+	let inputs = [];
+	let result = [];
+	return (x, y) => {
+		let x1, y1;
+		if (inputs[0] === x && inputs[1] === y) {
+			[ x1, y1 ] = result;
+			return [ x1, y1 ];
+		} else {
+			result = f(x, y);
+			inputs = [ x, y ];
+			return result;
+		}
+	};
 }

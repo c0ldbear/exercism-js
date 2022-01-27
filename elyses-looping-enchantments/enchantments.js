b257cf82ad6b7fa0d21ff5ffe0bb8d004b915dd8
teddy <teddyjh@protonmail.com>
2022-01-27 11:11:17 +0100

    add solution to Elyses Looping Enchantments

--- elyses-looping-enchantments/enchantments.js
+++ elyses-looping-enchantments/enchantments.js
@@ -9,7 +9,9 @@
  * @returns {number} number of cards of a single type there are in the deck
  */
 export function cardTypeCheck(stack, card) {
-  throw new Error('Implement the cardTypeCheck function');
+	let count = 0;
+	stack.forEach((c) => (c === card ? count++ : 0));
+	return count;
 }
 
 /**
@@ -20,5 +22,13 @@ export function cardTypeCheck(stack, card) {
  * @returns {number} number of cards that are either odd or even (depending on `type`)
  */
 export function determineOddEvenCards(stack, type) {
-  throw new Error('Implement the determineOddEvenCards function');
+	let count = 0;
+	for (const card of stack) {
+		if (type) {
+			card % 2 === 0 ? count++ : 0; // Check even
+		} else {
+			card % 2 !== 0 ? count++ : 0;
+		}
+	}
+	return count;
 }

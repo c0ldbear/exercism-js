d01490f1d755d5280f3c280caf5f27e9595e5859
teddy <teddyjh@protonmail.com>
2022-02-03 10:38:15 +0100

    add solution to RNA Transcription with an object and help function

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -3,6 +3,17 @@
 // convenience to get you started writing code faster.
 //
 
-export const toRna = () => {
-  throw new Error('Remove this statement and implement this function');
+export const toRna = (rna) => {
+	return rna !== '' ? transcribeRna(rna) : '';
 };
+
+const RNA = {
+	G: 'C',
+	C: 'G',
+	T: 'A',
+	A: 'U'
+};
+
+function transcribeRna(rna) {
+	return rna.split('').map((nuclide) => RNA[nuclide]).join('');
+}
